f305d9bc94e7804a3d6191ea718a1ddc1d697644
priyansh4993 <157016401+priyansh4993@users.noreply.github.com>
2024-01-25 21:54:35 +0530

    Add files via upload

---
+++ QR code generator/practice.js
@@ -0,0 +1,18 @@
+let imgbox = document.getElementById("imgbox")
+let qrimg = document.getElementById("qrimg")
+let qrtext = document.getElementById("qrtext")
+
+
+
+function generateQR(){
+    if(qrtext.value.length >0){
+  qrimg.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example "
+  + qrtext.value;
+  imgbox.classList.add("show-img");
+}else{
+    qrtext.classList.add("error");
+    setTimeout (()=>{
+        qrtext.classList.remove("error");
+    },1000);
+}
+}
